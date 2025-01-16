7559175fed69a555f317c1fb6bfc7eeda807203d
david castillo <seth.castillo4@outlook.com>
2025-01-15 19:02:39 -0600

    Finished Method Overloading exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace MethodOverloading
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MethodOverloading
 {
     public class Program
     {
+
+        public static int Add(int num1, int num2)
+	    {
+	        return num1 + num2;
+	    }
+	    
+	    public static decimal Add(decimal num1, decimal num2)
+	    {
+	        return num1 + num2;
+	    }
+	    
+	    public static string Add(int num1, int num2, bool Check)
+	    {
+	        var sum = num1 + num2;
+	        
+	        if(Check == true && sum > 1)
+	        {
+	            return $"{sum} dollars";
+	        }
+	        else if(Check == true && sum == 1)
+	        {
+	            return $"{sum} dollar";
+	        }
+	        else if(Check == true && sum < 1)
+	        {
+	            return $"{sum} dollars";
+	        }
+	        else
+	        {
+	            return sum.ToString();
+	        }
+	    }
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { 
+          Console.WriteLine(Add(1,2));
         }
     }
 }
